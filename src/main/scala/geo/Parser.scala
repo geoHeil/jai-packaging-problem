bedf62a93ac5c48052a30cd342aa22603d1d44db
Georg Heiler <georg.kf.heiler@gmail.com>
2017-06-03 08:53:00 +0200

    Check meta inf

--- src/main/scala/geo/Parser.scala
+++ src/main/scala/geo/Parser.scala
@@ -15,13 +15,22 @@ object Parser {
   @transient lazy val writer = new WKTWriter()
 
   def parse(rasterData: GridCoverage2D) = {
+    // check for missing manifest entries
+    // https://stackoverflow.com/questions/34352003/jai-javax-imageio-imageio-exception-while-running-exported-jar
+    // https://stackoverflow.com/questions/7051603/jai-vendorname-null
+    println("########################")
+    println("checking imageio native meta")
+    println(com.sun.media.imageioimpl.common.PackageUtil.getVendor())
+    println(com.sun.media.imageioimpl.common.PackageUtil.getVersion())
+    println(com.sun.media.imageioimpl.common.PackageUtil.getSpecificationTitle())
+    println("########################")
 
     // reclassify (bin values of raster
 
     val r1 = org.jaitools.numeric.Range.create(Integer.valueOf(-100), true, Integer.valueOf(-7), true)
     val r2 = org.jaitools.numeric.Range.create(Integer.valueOf(-7), true, Integer.valueOf(0), true)
 
-    val classificationRanges = Seq(r1, r2)
+    val classificationRanges = Seq(r2, r1)
 
     // store geometry as reusable WKT types
     val vectorizedFeatures = extractor.execute(rasterData, 0, true, null, null, classificationRanges, null).features
